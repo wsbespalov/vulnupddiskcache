a5c53c3740a0f1da05eeea6352daafa6189ec641
Dmitriy Bespalov <bespalovd@brainbankers.com>
2018-09-13 20:42:20 +0500

    make thread for cwe update, make callback at the end of thread work

--- src/plugins/plg_cwe_updater.py
+++ src/plugins/plg_cwe_updater.py
@@ -3,6 +3,7 @@ import sys
 import time
 import json
 import zlib
+from threading import Thread
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from datetime import datetime
@@ -26,35 +27,6 @@ if not os.path.exists(os.path.join(baseDir, CACHE_FOLDER + CWE_FOLDER)):
 
 import diskcache
 
-
-class JSONDisk(diskcache.Disk):
-    def __init__(self, directory, compress_level=1, **kwargs):
-        self.compress_level = compress_level
-        super(JSONDisk, self).__init__(directory, **kwargs)
-
-    def put(self, key):
-        json_bytes = json.dumps(key).encode('utf-8')
-        data = zlib.compress(json_bytes, self.compress_level)
-        return super(JSONDisk, self).put(data)
-
-    def get(self, key, raw):
-        data = super(JSONDisk, self).get(key, raw)
-        return json.loads(zlib.decompress(data).decode('utf-8'))
-
-    def store(self, value, read):
-        if not read:
-            json_bytes = json.dumps(value).encode('utf-8')
-            value = zlib.compress(json_bytes, self.compress_level)
-        return super(JSONDisk, self).store(value, read)
-
-    def fetch(self, mode, filename, value, read):
-        data = super(JSONDisk, self).fetch(mode, filename, value, read)
-        if not read:
-            data = json.loads(zlib.decompress(data).decode('utf-8'))
-        return data
-
-
-# dc = diskcache.Cache(CACHE_FOLDER + CWE_FOLDER, disk=JSONDisk, disk_compress_level=6)
 dc = diskcache.Deque()
 
 
@@ -136,7 +108,6 @@ class CWEUpdater(object):
         LOGINFO_IF_ENABLED(SOURCE_MODULE, '[+] Start job')
         parsed_items = self.update_cwe()
         if len(parsed_items) != 0:
-
             result = True
             return True
         else:
@@ -145,10 +116,73 @@ class CWEUpdater(object):
         LOGINFO_IF_ENABLED(SOURCE_MODULE, '[+] Complete job')
         return result
 
+################# 3
+
+class UPDCWEThreadClass(Thread):
+    def __init__(self, name, callback=None, callback_args=None, *args, **kwargs):
+        target = kwargs.pop('target')
+        super(UPDCWEThreadClass, self).__init__(target=self.target_with_callback, *args, **kwargs)
+        self.callback = callback
+        self.method = target
+        self.callback_args = callback_args
+
+    def target_with_callback(self):
+        self.method()
+        if self.callback is not None:
+            self.callback(*self.callback_args)
+
+def update_cwe_job():
+    parsed_items= []
+    if check_internet_connection():
+        parser = make_parser()
+        cwe_handler = CWEHandler()
+        parser.setContentHandler(cwe_handler)
+        source = SETTINGS.get("cwe", {}).get("source", "http://cwe.mitre.org/data/xml/cwec_v2.8.xml.zip")
+        try:
+            LOGINFO_IF_ENABLED(SOURCE_MODULE, '[+] Start downloading file')
+            data, response = get_file(getfile=source)
+            if 'error' not in response:
+                LOGINFO_IF_ENABLED(SOURCE_MODULE, '[+] Start parsing CWE data')
+                parser.parse(data)
+                LOGINFO_IF_ENABLED(SOURCE_MODULE, '[+] Complete parsing CWE data')
+                for cwe in cwe_handler.cwe:
+                    cwe['description_summary'] = cwe['description_summary'].replace("\t\t\t\t\t", " ")
+                    item = {'tag': 'cwe', 'state':'parsed', 'data': cwe}
+                    parsed_items.append(item)
+                    #
+                    dc.append(item)
+                    #
+                LOGINFO_IF_ENABLED(SOURCE_MODULE, "[===========================================================================]")
+                LOGINFO_IF_ENABLED(SOURCE_MODULE, '[+] CWE update complete at:'.format(datetime.utcnow()))
+                LOGINFO_IF_ENABLED(SOURCE_MODULE, '[+]     Processed:   {}'.format(len(parsed_items)))
+                LOGINFO_IF_ENABLED(SOURCE_MODULE, "[===========================================================================]")
+                return parsed_items
+            else:
+                LOGERR_IF_ENABLED(SOURCE_MODULE, '[-] There are some errors in server response: {}'.format(response))
+        except Exception as ex:
+            LOGERR_IF_ENABLED(SOURCE_MODULE, "Got exception during downloading CWE source: {0}".format(ex))
+            return []
+    else:
+        LOGERR_IF_ENABLED(SOURCE_MODULE, '[-] No internet connection!')
+        return []
 
 
+def cwe_complete_callback(args):
+    print('callback with args: {}'.format(args))
+    print(len(dc))
 
-from threading import Thread
+def test3():
+    print('start test 3')
+    thr = UPDCWEThreadClass(
+        name='UPDCWEThreadClass',
+        target=update_cwe_job,
+        callback=cwe_complete_callback,
+        callback_args=('test 3 args',)
+        )
+    thr.start()
+
+################# 2
+
 
 
 class UPDCWE(Thread):
@@ -187,6 +221,7 @@ def test2():
         )
     thr.start()
 
+################# 1
 
 class SetThread(Thread):
     def __init__(self, name):
@@ -222,6 +257,8 @@ def main(argv):
             test()
         elif argv[0] == 'test2':
             test2()
+        elif argv[0] == 'test3':
+            test3()
     else:
         cweUpdater = CWEUpdater(argv)
         cweUpdater.job()
